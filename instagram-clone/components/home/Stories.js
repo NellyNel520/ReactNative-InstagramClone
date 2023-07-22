67333c90b2fc495b6056df80af8ce185e6cbccd9
NellyNel520 <channelharris520@gmail.com>
2023-07-22 19:57:43 -0400

    created stories component and added it to the homescreen

---
+++ instagram-clone/components/home/Stories.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Stories = () => {
+  return (
+    <View>
+      <Text style={{color: 'white'}}>Stories</Text>
+    </View>
+  )
+}
+
+export default Stories
--- instagram-clone/screens/HomeScreen.js
+++ instagram-clone/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
 import React from 'react'
 import Header from '../components/home/Header'
+import Stories from '../components/home/Stories'
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <Stories />
     </SafeAreaView>
   )
 }
