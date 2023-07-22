a271d50caa7e825c31a0c43e6e14adcbed4364dd
NellyNel520 <channelharris520@gmail.com>
2023-07-22 19:52:51 -0400

    added heart and messenger icon to home screen header

--- instagram-clone/components/home/Header.js
+++ instagram-clone/components/home/Header.js
@@ -1,12 +1,47 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native'
 import React from 'react'
 
 const Header = () => {
   return (
     <View>
-      <Text style={{color: 'white'}}>Header</Text>
+      {/* Logo */}
+      <TouchableOpacity>
+        <Image style={styles.logo} source={require('../../assets/ig-logo.png')}/>
+      </TouchableOpacity>
+
+      {/* icons on right */}
+      <View>
+        <TouchableOpacity>
+          <Image style={styles.icon} source={{uri: 'https://img.icons8.com/material-outlined/24/ffffff/like--v1.png'}}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Image 
+            style={styles.icon}
+            source={{uri: 'https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/24/ffffff/external-messenger-social-media-tanah-basah-basic-outline-tanah-basah.png'}}
+          />
+        </TouchableOpacity> 
+
+
+      </View>
+
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  logo: {
+		width: 100,
+		height: 50,
+		resizeMode: 'contain',
+	},
+  icon: {
+		width: 30,
+		height: 30,
+		marginLeft: 14,
+		resizeMode: 'contain',
+	},
+})
 
 export default Header
