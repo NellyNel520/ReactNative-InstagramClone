41951b51fb1df82fc0d852f224eebdc9dabb8c22
NellyNel520 <channelharris520@gmail.com>
2023-07-22 21:58:17 -0400

    completed post image section

--- instagram-clone/components/home/Post.js
+++ instagram-clone/components/home/Post.js
@@ -29,6 +29,7 @@ const Post = ({post}) => {
     <View>
       <Divider width={1} orientation='vertical'/>
       <PostHeader post={post} />
+      <PostImage post={post}/>
     </View>
   )
 }
@@ -40,6 +41,12 @@ const PostHeader = ({post}) => (
       <Image source={{ uri: post.profilePic}} style={styles.story}/>
       <Text style={{ color: 'white', marginLeft: 5, fontWeight: '700'}}>{post.user}</Text>
     </View>
+  </View>
+)
+
+const PostImage = ({post}) => (
+  <View style={{width: '100%', height: 450 }}>
+    <Image source={{uri: post.imageUrl}} style={{ height: '100%', resizeMode: 'cover'}}/>
   </View>
 )
 
