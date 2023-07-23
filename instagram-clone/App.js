d5aea033fc3fd86ee673f104f6ab187af3068792
NellyNel520 <channelharris520@gmail.com>
2023-07-23 00:58:07 -0400

    added signed in stack to app .js for screen navigation

--- instagram-clone/App.js
+++ instagram-clone/App.js
@@ -1,10 +1,10 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import HomeScreen from './screens/HomeScreen'
 import NewPostScreen from './screens/NewPostScreen'
+import SignedInStack from './navigation'
 
 export default function App() {
 	return (
-		<NewPostScreen />
-			// <HomeScreen />
+		<SignedInStack />
 	)
 }
