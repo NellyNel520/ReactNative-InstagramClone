a1de41fdc44b5cfaa5a8363598535066660f9269
NellyNel520 <channelharris520@gmail.com>
2023-07-22 21:37:55 -0400

    added post footer icons url for the like comment save and share icons

--- instagram-clone/components/home/Post.js
+++ instagram-clone/components/home/Post.js
@@ -1,6 +1,28 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+const postFooterIcons = [
+	{
+		name: 'Like',
+		imageUrl: 'https://img.icons8.com/ios/50/ffffff/like--v1.png',
+		likedImageUrl: 'https://img.icons8.com/ios-filled/50/F32424/like--v1.png',
+	},
+	{
+		name: 'Comment',
+		imageUrl: 'https://img.icons8.com/ios/50/ffffff/speech-bubble--v1.png',
+	},
+	{
+		name: 'Share',
+		imageUrl: 'https://img.icons8.com/ios/50/ffffff/speech-bubble--v1.png',
+	},
+	{
+		name: 'Save',
+		imageUrl:
+			'https://img.icons8.com/fluency-systems-regular/48/ffffff/bookmark-ribbon--v1.png',
+	},
+]
+
+
 const Post = () => {
   return (
     <View>
