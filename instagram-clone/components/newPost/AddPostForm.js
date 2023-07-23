6842717c71ae596ffba22f522ec4b437c1c14be1
NellyNel520 <channelharris520@gmail.com>
2023-07-23 00:05:37 -0400

    added placeholder image to form

--- instagram-clone/components/newPost/AddPostForm.js
+++ instagram-clone/components/newPost/AddPostForm.js
@@ -1,5 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput, Button  } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Divider } from 'react-native-elements'
+
+import * as Yup from 'yup'
+import { Formik } from 'formik'
+import validUrl from 'valid-url'
+
+
+const PLACEHOLDER_IMG =
+	'https://t3.ftcdn.net/jpg/02/68/55/60/360_F_268556012_c1WBaKFN5rjRxR2eyV33znK4qnYeKZjm.jpg'
+
 
 const AddPostForm = () => {
   return (
