b19ab6eb951be622afbbad7344943636d01fc25a
NellyNel520 <channelharris520@gmail.com>
2023-07-22 21:23:55 -0400

    added story images to home screen

--- instagram-clone/components/home/Stories.js
+++ instagram-clone/components/home/Stories.js
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import { USERS } from '../../data/users'
 
 const Stories = () => {
   return (
-    <View>
-      <Text style={{color: 'white'}}>Stories</Text>
+    <View style={{ marginBottom: 13}}>
+      {USERS.map((story, index) => (
+        <View key={index}>
+          <Image source={{uri: story.image}} style={styles.story}/>
+
+          <Text></Text>
+        </View>
+      ))}
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  story: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    marginLeft: 18,
+    borderWidth: 3,
+    borderColor: '#ff8501'
+
+  },
+})
 export default Stories
