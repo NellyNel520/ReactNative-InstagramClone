ef149670d2523bf8064f0da24a454073e6391848
NellyNel520 <channelharris520@gmail.com>
2023-07-23 00:14:32 -0400

    intalled formik and yup for form validation

--- instagram-clone/components/newPost/AddPostForm.js
+++ instagram-clone/components/newPost/AddPostForm.js
@@ -10,12 +10,46 @@ import validUrl from 'valid-url'
 const PLACEHOLDER_IMG =
 	'https://t3.ftcdn.net/jpg/02/68/55/60/360_F_268556012_c1WBaKFN5rjRxR2eyV33znK4qnYeKZjm.jpg'
 
+  const uploadPostSchema = Yup.object().shape({
+    imageUrl: Yup.string().url().required('A URL is required'),
+    caption: Yup.string().max(2200, 'Caption has reached the character limit'),
+  })
 
 const AddPostForm = () => {
+  const [thumbnailUrl, setThumbnailUrl] = useState(PLACEHOLDER_IMG)
+
+
   return (
-    <View>
-      <Text style={{color: 'white'}}>AddPostForm</Text>
-    </View>
+    <Formik>
+    {({
+				handleBlur,
+				handleChange,
+				handleSubmit,
+				values,
+				errors,
+				isValid,
+			}) => (
+        <>
+          <View>
+            <Image source={{
+              uri: validUrl.isUri(thumbnailUrl)
+              ? thumbnailUrl
+              : PLACEHOLDER_IMG
+            }}
+              style={{width: 100, height: 100}}
+            />
+            <View>
+              <TextInput />
+            </View>
+
+            <Divider />
+            <TextInput/>
+
+          </View>
+        </>
+      )}
+      
+    </Formik>
   )
 }
 
