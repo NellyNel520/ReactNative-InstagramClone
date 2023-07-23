113130a1df21e3d4f44a64cfa8d7021595187022
NellyNel520 <channelharris520@gmail.com>
2023-07-22 21:35:43 -0400

    created post component and added posts data to the home screen to map and display all posts

--- instagram-clone/screens/HomeScreen.js
+++ instagram-clone/screens/HomeScreen.js
@@ -1,13 +1,21 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import Header from '../components/home/Header'
 import Stories from '../components/home/Stories'
+import { POSTS } from '../data/posts'
+import Post from '../components/home/Post'
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
       <Stories />
+
+      <ScrollView>
+        {POSTS.map((post, index) => (
+          <Post key={index}/>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   )
 }
