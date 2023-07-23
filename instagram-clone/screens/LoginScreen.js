98fb7c12d21fe09cac50c3bd7ef27bdaa04266c9
NellyNel520 <channelharris520@gmail.com>
2023-07-23 01:25:02 -0400

    added instagram logo to login screen

--- instagram-clone/screens/LoginScreen.js
+++ instagram-clone/screens/LoginScreen.js
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
+
+const INSTAGRAM_LOGO = 'https://img.icons8.com/fluency/48/instagram-new.png'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={{uri: INSTAGRAM_LOGO, height: 100, width: 100}} />
+      </View>
+      {/* Login Form */}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 50,
+    paddingHorizontal: 12,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginTop: 60,
+  }
+})
 
 export default LoginScreen
