17edcc969e15d6587499781d7cfb54a32863779d
NellyNel520 <channelharris520@gmail.com>
2023-07-23 02:45:19 -0400

    completed sign up form layout

--- instagram-clone/components/signup/SignupForm.js
+++ instagram-clone/components/signup/SignupForm.js
@@ -12,8 +12,6 @@ import * as Yup from 'yup'
 import { Formik } from 'formik'
 import Validator from 'email-validator'
 
-
-
 const signupFormSchema = Yup.object().shape({
 	email: Yup.string().email().required('An email'),
 	username: Yup.string().required().min(2, 'A username is required'),
@@ -23,13 +21,148 @@ const signupFormSchema = Yup.object().shape({
 })
 
 const SignupForm = () => {
-  return (
-    <View>
-      <Formik>
-        
-      </Formik>
-    </View>
-  )
+	return (
+		<View style={styles.wrapper}>
+			<Formik
+				initialValues={{ email: '', password: '', username: '' }}
+				onSubmit={(values) => {
+					// onSignup(values.email, values.password, values.username)
+          console.log(values.email, values.password, values.username)
+				}}
+				validationSchema={signupFormSchema}
+				validateOnMount={true}
+			>
+				{({
+					handleChange,
+					handleBlur,
+					handleSubmit,
+					values,
+					errors,
+					isValid,
+				}) => (
+					<>
+						<View
+            style={[
+								styles.inputField,
+								{
+									borderColor:
+										values.email.length < 1 || Validator.validate(values.email)
+											? '#ccc'
+											: 'red',
+								},
+							]}
+            >
+							<TextInput
+								placeholderTextColor="#444"
+								placeholder="Email"
+								autoCapitalize="none"
+								keyboardType="email-address"
+								textContentType="emailAddress"
+								autoFocus={true}
+								onChangeText={handleChange('email')}
+								onBlur={handleBlur('email')}
+								value={values.email}
+							/>
+						</View>
+
+						<View style={[
+								styles.inputField,
+								{
+									borderColor:
+										values.username.length || values.username.length > 6
+											? '#ccc'
+											: 'red',
+								},
+							]}>
+							<TextInput
+								placeholderTextColor="#444"
+								placeholder="Username"
+								autoCapitalize="none"
+								textContentType="username"
+								onChangeText={handleChange('username')}
+								onBlur={handleBlur('username')}
+								value={values.username}
+							/>
+						</View>
+
+						<View style={[
+								styles.inputField,
+								{
+									borderColor:
+										values.password.length || values.password.length > 6
+											? '#ccc'
+											: 'red',
+								},
+							]}>
+							<TextInput
+								placeholderTextColor="#444"
+								placeholder="Password"
+								autoCapitalize="none"
+								autoCorrect={false}
+								secureTextEntry={true}
+								textContentType="password"
+								onChangeText={handleChange('password')}
+								onBlur={handleBlur('password')}
+								value={values.password}
+							/>
+						</View>
+
+						<View style={{ alignItems: 'flex-end', marginBottom: 30 }}>
+							<Text style={{ color: '#6BB0F5' }}>Forgot Password?</Text>
+						</View>
+
+						<Pressable
+							titleSize={20}
+							style={styles.button(isValid)}
+							onPress={handleSubmit}
+							disabled={!isValid}
+						>
+							<Text style={styles.buttonText}>Sign Up</Text>
+						</Pressable>
+
+            <View style={styles.signupContainer}>
+            <Text style={{color: 'white'}}>Already have an account?</Text>
+            <TouchableOpacity>
+              <Text style={{ color: '#6BB0F5' }}>  Login</Text>
+            </TouchableOpacity>
+
+            </View>
+					</>
+				)}
+			</Formik>
+		</View>
+	)
 }
+
+const styles = StyleSheet.create({
+	inputField: {
+		borderRadius: 4,
+		padding: 12,
+		backgroundColor: '#FAFAFA',
+		marginBottom: 10,
+		borderWidth: 1,
+	},
+	wrapper: {
+		marginTop: 80,
+	},
+	button: (isValid) => ({
+		backgroundColor: isValid ? '#0096f6' : '#9ACAF7',
+		alignItems: 'center',
+		justifyContent: 'center',
+		minHeight: 42,
+		borderRadius: 4,
+	}),
+	buttonText: {
+		fontWeight: 600,
+		color: '#fff',
+		fontSize: 20,
+	},
+	signupContainer: {
+		flexDirection: 'row',
+		width: '100%',
+		justifyContent: 'center',
+		marginTop: 50,
+	},
+})
 
 export default SignupForm
