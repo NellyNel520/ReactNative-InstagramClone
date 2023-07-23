639a9b19480a2dc42b47bde90fd202a4e7b16f67
NellyNel520 <channelharris520@gmail.com>
2023-07-23 04:23:03 -0400

    added authnavigation to app.js file

--- instagram-clone/App.js
+++ instagram-clone/App.js
@@ -2,9 +2,10 @@ import { View, Text, SafeAreaView } from 'react-native'
 import HomeScreen from './screens/HomeScreen'
 import NewPostScreen from './screens/NewPostScreen'
 import SignedInStack from './navigation'
+import AuthNavigation from './AuthNavigation'
 
 export default function App() {
 	return (
-		<SignedInStack />
+		<AuthNavigation />
 	)
 }
