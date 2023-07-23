fb1b5daefac9ed8a21b15e3cdf5d0ee1286f06dd
NellyNel520 <channelharris520@gmail.com>
2023-07-22 23:38:44 -0400

    created new post screen for post upload

--- instagram-clone/App.js
+++ instagram-clone/App.js
@@ -1,8 +1,10 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import HomeScreen from './screens/HomeScreen'
+import NewPostScreen from './screens/NewPostScreen'
 
 export default function App() {
 	return (
-			<HomeScreen />
+		<NewPostScreen />
+			// <HomeScreen />
 	)
 }
---
+++ instagram-clone/screens/NewPostScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NewPostScreen = () => {
+  return (
+    <View>
+      <Text>NewPostScreen</Text>
+    </View>
+  )
+}
+
+export default NewPostScreen
