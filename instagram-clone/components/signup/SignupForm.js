741fe01de3e4068be7c4dbe64912211e2091b04a
NellyNel520 <channelharris520@gmail.com>
2023-07-23 02:32:08 -0400

    created signup form schema for form validation

--- instagram-clone/components/signup/SignupForm.js
+++ instagram-clone/components/signup/SignupForm.js
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native'
+import {
+	View,
+	Text,
+	TextInput,
+	Button,
+	StyleSheet,
+	Pressable,
+	TouchableOpacity,
+} from 'react-native'
 import React from 'react'
+import * as Yup from 'yup'
+import { Formik } from 'formik'
+import Validator from 'email-validator'
+
+
+
+const signupFormSchema = Yup.object().shape({
+	email: Yup.string().email().required('An email'),
+	username: Yup.string().required().min(2, 'A username is required'),
+	password: Yup.string()
+		.required()
+		.min(6, 'Your password has to have at least 8 characters '),
+})
 
 const SignupForm = () => {
   return (
     <View>
-      <Text style={{color: 'white'}}>SignupForm</Text>
+      <Formik>
+        
+      </Formik>
     </View>
   )
 }
