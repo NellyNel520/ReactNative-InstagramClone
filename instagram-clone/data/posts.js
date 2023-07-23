5d10a7c7a4b2cb9aae044dd4ff876c9452d754ed
NellyNel520 <channelharris520@gmail.com>
2023-07-22 21:10:50 -0400

    created data folder with post and users files for filler data

---
+++ instagram-clone/data/posts.js
@@ -0,0 +1,83 @@
+import { USERS } from './users'
+export const POSTS = [
+	{
+		imageUrl: 'https://images.unsplash.com/photo-1509281373149-e957c6296406?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+		user: USERS[0].user,
+		likes: 7870,
+		caption: 'Invent your world. Surround yourself with people, color, sounds, and work that nourish you.',
+		profilePic: USERS[0].image,
+		comments: [
+			{
+				user: 'Ragnar.Mahlangu5',
+				comment: 'The fashion game is strong in this one! You always manage to slay every outfit. Teach me your style secrets, please!',
+			},
+			{
+				user: 'Manfred.David81',
+				comment: 'fire bro',
+			},
+			{
+				user: 'Bin.Green42',
+				comment: 'This picture is pure art! The composition and colors are absolutely stunning. You have such a great eye for photography!',
+			},
+		]
+	},
+	{
+		imageUrl: 'https://plus.unsplash.com/premium_photo-1669741908308-5ca216f3fcd1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+		user: USERS[1].user,
+		likes: 17250,
+		caption: 'Invent your world. Surround yourself with people, color, sounds, and work that nourish you.',
+		profilePic: USERS[1].image,
+		comments: [
+			{
+				user: 'Ragnar.Mahlangu5',
+				comment: 'The fashion game is strong in this one! You always manage to slay every outfit. Teach me your style secrets, please!',
+			},
+			{
+				user: 'Manfred.David81',
+				comment: 'fire bro',
+			},
+			{
+				user: 'Bin.Green42',
+				comment: 'This picture is pure art! The composition and colors are absolutely stunning. You have such a great eye for photography!',
+			},
+		]
+	},
+	{
+		imageUrl: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+		user: USERS[2].user,
+		likes: 400,
+		caption: 'Invent your world. Surround yourself with people, color, sounds, and work that nourish you.',
+		profilePic: USERS[2].image,
+		comments: [
+			{
+				user: 'Ragnar.Mahlangu5',
+				comment: 'The fashion game is strong in this one! You always manage to slay every outfit. Teach me your style secrets, please!',
+			},
+			{
+				user: 'Bin.Green42',
+				comment: 'This picture is pure art! The composition and colors are absolutely stunning. You have such a great eye for photography!',
+			},
+		]
+	},
+	{
+		imageUrl: 'https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+		user: USERS[3].user,
+		likes: 7870,
+		caption: 'Invent your world. Surround yourself with people, color, sounds, and work that nourish you.',
+		profilePic: USERS[3].image,
+		comments: [
+			{
+				user: 'Ragnar.Mahlangu5',
+				comment: 'The fashion game is strong in this one! You always manage to slay every outfit. Teach me your style secrets, please!',
+			},
+			{
+				user: 'Manfred.David81',
+				comment: 'fire bro',
+			},
+			{
+				user: 'Bin.Green42',
+				comment: 'This picture is pure art! The composition and colors are absolutely stunning. You have such a great eye for photography!',
+			},
+		]
+	},
+]
---
+++ instagram-clone/data/users.js
@@ -0,0 +1,42 @@
+export const USERS = [
+	{
+		user: 'nellyNel520',
+		image:
+			'https://images.unsplash.com/photo-1577565177023-d0f29c354b69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aGVhZCUyMHNob3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+	},
+  {
+		user: 'sugarBabie',
+		image:
+			'https://images.unsplash.com/photo-1577806934037-32d94e326e84?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGhlYWQlMjBzaG90c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+	},
+  {
+		user: 'markTwane33',
+		image: 'https://plus.unsplash.com/premium_photo-1688739352540-a75b102d8551?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGhlYWQlMjBzaG90c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+	},
+  {
+		user: 'AtypicalGal',
+		image:
+			'https://images.unsplash.com/photo-1504887823384-08d44627fb28?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhlYWQlMjBzaG90c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+	},
+  {
+		user: 'DaDonDaDa_blala',
+		image:
+			'https://images.unsplash.com/photo-1591853725932-79227eb926b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGhlYWQlMjBzaG90c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+	},
+  {
+		user: 'markTwane33',
+		image: 'https://plus.unsplash.com/premium_photo-1688739352540-a75b102d8551?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGhlYWQlMjBzaG90c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+	},
+  {
+		user: 'AtypicalGal',
+		image:
+			'https://images.unsplash.com/photo-1504887823384-08d44627fb28?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhlYWQlMjBzaG90c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+	},
+  {
+		user: 'DaDonDaDa_blala',
+		image:
+			'https://images.unsplash.com/photo-1591853725932-79227eb926b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGhlYWQlMjBzaG90c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+	},
+
+
+]
